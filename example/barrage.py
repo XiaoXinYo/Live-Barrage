da5235ff1271e23f4cfe8329e668371b2df2b9ff
xiaoxinyo <1104361313@qq.com>
2022-07-24 23:49:16 +0800

    null

---
+++ example/barrage.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# Author: XiaoXinYo
+
+import asyncio
+import websockets
+import json
+
+async def receive(websocket):
+    while True:
+        data = await websocket.recv()
+        data = json.loads(data)
+        for data_count in data:
+            single_data = data[data_count]
+            print("{:10s} | {:10s} | {:10s} | {:10s}".format(data_count, single_data.get('type'), single_data.get('username'), single_data.get('content')))
+
+async def run(websocket):
+    while True:
+        try:
+            await receive(websocket)
+        except websockets.ConnectionClosed:
+            print("断开连接")
+            break
+       
+if __name__ == '__main__':
+    asyncio.get_event_loop().run_until_complete(websockets.serve(run, "127.0.0.1", 5000))
+    asyncio.get_event_loop().run_forever()
