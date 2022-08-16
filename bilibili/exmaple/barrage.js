2652873ee2e8da72795285dbd1f2dc6cbfd840d2
Coaixy <coaixy@outlook.com>
2022-08-16 21:31:21 +0800

    add(Bilibili JavaScript Demo)

--- bilibili/exmaple/barrage.js
+++ bilibili/exmaple/barrage.js
@@ -3,7 +3,8 @@ let ADDRES = "ws://127.0.0.1:5000"
 let ws = new WebSocket(ADDRES)
 ws.onopen = function() {
 	console.log("连接成功")
-	ws.send("8224637")
+	// ws.send("8224637") 房间号
+    ws.send("https://live.bilibili.com/8224637") //网址
 }
 ws.onmessage = function(e) {
 	console.log(e.data)
