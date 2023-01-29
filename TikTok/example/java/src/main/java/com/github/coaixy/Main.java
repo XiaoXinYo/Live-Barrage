139a051619f7f11a0d92291a3279b14adabd544d
Coaixy <coaixy@outlook.com>
2023-01-29 10:50:51 +0800

    删除错误的示例

--- TikTok/example/java/src/main/java/com/github/coaixy/Main.java
+++
@@ -1,9 +0,0 @@
-package com.github.coaixy;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-        Socket server = new Socket(5000);
-        server.run();
-    }
-}
